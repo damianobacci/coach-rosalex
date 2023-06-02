0fd3676c07031ea901969e5edf19a76fc7767e4d
Damiano <damiano.bacci@gmail.com>
2023-06-02 17:11:03 +0200

    Working seconds

--- javascript30/css-js-clock/script.js
+++ javascript30/css-js-clock/script.js
@@ -1,5 +1,15 @@
-let now = new Date();
+const secondHand = document.querySelector(".second-hand");
 
-let hours = now.getHours();
-let minutes = now.getMinutes();
-let seconds = now.getSeconds();
+function drawClock() {
+  let now = new Date();
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  let secondsDegree = (seconds * 360) / 60 + 90;
+  secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+  console.log(seconds);
+}
+
+setInterval(drawClock, 1000);
