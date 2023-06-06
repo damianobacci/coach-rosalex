db998f96e3016a51f552285896d3d0168db3bbc2
Damiano <damiano.bacci@gmail.com>
2023-06-06 11:49:02 +0200

    new exercise in the playground

--- css-js-playground/hide-on-click/script.js
+++
@@ -1,6 +0,0 @@
-let text = document.getElementById("text");
-let button = document.querySelector("button");
-let button2 = document.querySelectorAll("button")[1];
-
-button.addEventListener("click", () => (text.style.display = "none"));
-button2.addEventListener("click", () => (text.style.display = "block"));
