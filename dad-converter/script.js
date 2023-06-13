f295b494ac8aac464cd99c14113ce3fec301c9f5
Damiano <damiano.bacci@gmail.com>
2023-06-13 13:49:12 +0200

    Added the dad-millennial converter

---
+++ dad-converter/script.js
@@ -0,0 +1,29 @@
+const emojis =["😍", "😘", "😂", "🤣", "😆", "😁"]
+
+function removeFullStop(text) {
+    if (text.endsWith(".")) {
+      text = text.slice(0, -1);
+    }
+    return text;
+  }
+
+function convertText() {
+    const message = document.getElementById('boomer');
+    if (!message.value) {
+        message.placeholder = "Yooooo insert a message first!! 👈👈"
+    } else {
+    let text = message.value;
+    if (text.endsWith("!")) {
+        text = text.slice(0, -1);
+    };
+    const randomIndex = Math.floor(Math.random() * emojis.length);
+    const randomEmoji = emojis[randomIndex];
+    text = removeFullStop(text);
+    let converted = text += "! " + randomEmoji;
+    let textArea = document.getElementById("millennial");
+    textArea.textContent = converted;
+    };
+}
+const button = document.getElementById('convert');
+button.addEventListener("click", convertText);
+
