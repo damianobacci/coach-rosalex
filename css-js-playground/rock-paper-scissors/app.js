7b63923551a9548f452da444acadb94f705dcfb2
Damiano <damiano.bacci@gmail.com>
2023-06-16 12:00:26 +0200

    Rock-Paper-Scissors game

---
+++ css-js-playground/rock-paper-scissors/app.js
@@ -0,0 +1,8 @@
+const MOVES = ["Rock", "Paper", "Scissors"];
+
+let randomIndex = Math.floor(Math.random() * MOVES.length);
+let randomMove = MOVES[randomIndex];
+
+console.log(randomMove);
+
+let myChoice = alert("What?");
