35b66d6e498bfe5aae3f390c4c17e2f3542785f6
Damiano Bacci <damiano.bacci@gmail.com>
2023-08-28 20:18:09 +0200

    New React Component using create-react-app

---
+++ u64-react-tab/u64-tabs/src/App.js
@@ -0,0 +1,84 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useState } from "react";
+
+const content = [
+  [
+    "React is extremely popular",
+    "It makes building complex, interactive UIs a breeze",
+    "It's powerful & flexible",
+    "It has a very active and versatile ecosystem",
+  ],
+  [
+    "Components, JSX & Props",
+    "State",
+    "Hooks (e.g., useEffect())",
+    "Dynamic rendering",
+  ],
+  [
+    "Official web page (react.dev)",
+    "Next.js (Fullstack framework)",
+    "React Native (build native mobile apps with React)",
+  ],
+  [
+    "Vanilla JavaScript requires imperative programming",
+    "Imperative Programming: You define all the steps needed to achieve a result",
+    "React on the other hand embraces declarative programming",
+    "With React, you define the goal and React figures out how to get there",
+  ],
+];
+
+function App() {
+  const [activeTab, setActiveTab] = useState(0);
+
+  return (
+    <div>
+      <header>
+        <img src="react-logo-xs.png" alt="React logo" />
+        <div>
+          <h1>Unseen64</h1>
+          <p>The greatest website about beta videogames</p>
+        </div>
+      </header>
+
+      <div id="tabs">
+        <menu>
+          <button
+            className={activeTab === 0 ? "active" : ""}
+            onClick={() => setActiveTab(0)}
+          >
+            Why Unseen64?
+          </button>
+          <button
+            className={activeTab === 1 ? "active" : ""}
+            onClick={() => setActiveTab(1)}
+          >
+            Core Features
+          </button>
+          <button
+            className={activeTab === 2 ? "active" : ""}
+            onClick={() => setActiveTab(2)}
+          >
+            Related Resources
+          </button>
+          <button
+            className={activeTab === 3 ? "active" : ""}
+            onClick={() => setActiveTab(3)}
+          >
+            React VS JS
+          </button>
+        </menu>
+        <div id="tab-content">
+          <ul>
+            {content[activeTab].map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
