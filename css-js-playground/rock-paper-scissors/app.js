70ecbf027f24611707f1e75e5e6f325871329c0c
Damiano Bacci <damiano.bacci@gmail.com>
2023-08-29 10:46:35 +0200

    Updated the logic of rock-paper-scissors game

--- css-js-playground/rock-paper-scissors/app.js
+++ css-js-playground/rock-paper-scissors/app.js
@@ -1,8 +1,39 @@
-const MOVES = ["Rock", "Paper", "Scissors"];
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const yourChoiceDisplay = document.getElementById("your-choice");
+const resultDisplay = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
 
-let randomIndex = Math.floor(Math.random() * MOVES.length);
-let randomMove = MOVES[randomIndex];
+let randomMove;
 
-console.log(randomMove);
+function generateComputerChoice() {
+  const MOVES = ["Rock", "Paper", "Scissors"];
 
-let myChoice = alert("What?");
+  let randomIndex = Math.floor(Math.random() * MOVES.length);
+  randomMove = MOVES[randomIndex];
+  computerChoiceDisplay.innerHTML = randomMove;
+}
+
+function determineWin() {
+  if (myChoice === "Rock" && randomMove === "Scissors") {
+    resultDisplay.innerHTML = "You win!";
+  } else if (myChoice === "Paper" && randomMove === "Rock") {
+    resultDisplay.innerHTML = "You win!";
+  } else if (myChoice === "Scissors" && randomMove === "Paper") {
+    resultDisplay.innerHTML = "You win!";
+  } else if (myChoice === randomMove) {
+    resultDisplay.innerHTML = "Draw!";
+  } else {
+    resultDisplay.innerHTML = "You lose!";
+  }
+}
+
+let myChoice;
+possibleChoices.forEach((choice) =>
+  choice.addEventListener("click", (e) => {
+    resultDisplay.innerHTML = "";
+    myChoice = e.target.id;
+    yourChoiceDisplay.innerHTML = myChoice;
+    generateComputerChoice();
+    determineWin();
+  })
+);
