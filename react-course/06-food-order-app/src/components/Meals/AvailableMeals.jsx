6db3aefbb1b967dbb50ef0c94ac33a89e9e4085e
Damiano Bacci <damiano.bacci@gmail.com>
2023-10-12 19:24:50 +0200

    Added a form to submit the cart

--- react-course/06-food-order-app/src/components/Meals/AvailableMeals.jsx
+++ react-course/06-food-order-app/src/components/Meals/AvailableMeals.jsx
@@ -7,12 +7,18 @@ import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(undefined);
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://react-http-a6dcf-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
       const responseData = await response.json();
       const loadedMeals = [];
 
@@ -25,9 +31,30 @@ const AvailableMeals = () => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
   const mealsList = meals.map((meal) => {
     return (
       <MealItem
