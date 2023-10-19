872290281510002fd51b338aeabaedb24eb9d45d
Damiano Bacci <damiano.bacci@gmail.com>
2023-10-19 17:30:36 +0200

    Typescript training

---
+++ typescript-training/base.ts
@@ -0,0 +1,21 @@
+let age: number = 12;
+
+let userName: string = "Max";
+
+let isTeacher: boolean = true;
+let hobbies: string[];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Coso",
+  age: 23,
+};
+
+let people: {
+  name: string;
+  age: number;
+}[];
