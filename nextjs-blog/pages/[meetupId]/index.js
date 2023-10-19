f621fc46ea71e25b33b69dbaffac04ff819b97c6
Damiano Bacci <damiano.bacci@gmail.com>
2023-10-19 09:35:36 +0200

    Nextjs training

---
+++ nextjs-blog/pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+function MeetupDetails() {
+  return (
+    <>
+      <img src="" alt="" />
+      <h1>First meetup</h1>
+      <address>Address</address>
+      <p>Meetup description</p>
+    </>
+  );
+}
+
+export default MeetupDetails;
