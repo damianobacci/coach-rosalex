cfd6475c50eeef4db24f8f54e17047ce3931fc2c
Damiano Bacci <damiano.bacci@gmail.com>
2023-10-29 17:02:33 +0100

    Express integration

--- node-course/app.js
+++ node-course/app.js
@@ -1,7 +1,7 @@
 const http = require("http");
+const express = require("express");
 
-const routes = require("./routes");
-
+const app = express();
 const server = http.createServer(routes);
 
 server.listen(3000);
