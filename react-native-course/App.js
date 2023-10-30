03ec4bf41b261357def3bc034909bbdac7824d5b
Damiano Bacci <damiano.bacci@gmail.com>
2023-10-30 11:51:50 +0100

    React Native course

---
+++ react-native-course/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Premi il bottone segreto</Text>
+      <StatusBar style="auto" />
+      <Button
+        onPress={() => {
+          alert("Il nano rulla");
+        }}
+        title="Premi"
+      ></Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
