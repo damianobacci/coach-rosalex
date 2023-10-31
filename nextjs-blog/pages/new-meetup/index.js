b35df443477fcc741a4073523ad155408ef52484
Damiano Bacci <damiano.bacci@gmail.com>
2023-10-31 15:27:48 +0100

    Next course

--- nextjs-blog/pages/new-meetup/index.js
+++
@@ -1,8 +0,0 @@
-import NewMeetupForm from "../../components/meetups/NewMeetupForm";
-
-function NewMeetupPage() {
-  function addMeetupHandler(enteredMeetupData) {}
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
-}
-
-export default NewMeetupPage;
