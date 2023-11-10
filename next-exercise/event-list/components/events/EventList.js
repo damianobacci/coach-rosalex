7f90cf8388614b0a0f3da33d092532ab3704c768
Damiano Bacci <damiano.bacci@gmail.com>
2023-11-10 23:10:09 +0100

    Small fix

--- next-exercise/event-list/components/events/EventList.js
+++ next-exercise/event-list/components/events/EventList.js
@@ -2,13 +2,11 @@ import EventItem from "./EventItem";
 
 const EventList = ({ items }) => {
   return (
-    <>
-      <ul>
-        {items.map((item) => (
-          <EventItem />
-        ))}
-      </ul>
-    </>
+    <ul>
+      {items.map((item) => (
+        <EventItem />
+      ))}
+    </ul>
   );
 };
 
